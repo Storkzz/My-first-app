280accaccf8b781f33812580cbbc921a3c00f41c
Shimin <shimin.sun@miles.no>
2023-12-31 01:48:50 +0100

    get your first conflicts bitch

--- first_webpage/src/app/app.component.ts
+++ first_webpage/src/app/app.component.ts
@@ -11,20 +11,29 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Stork is cool';
+  sideBoxWidth = "300px";
+  sideBoxButtonWidth = "300px";
 
   storkMode:string = "base mode";
-  
+
   public onButtonClick(mode : string){
+    if(this.sideBoxWidth === "0px"){
+      this.sideBoxWidth = "300px";
+    }
+     else if(this.sideBoxWidth === "300px"){
+      this.sideBoxWidth = "0px";
+    }
+
     if(mode === "base mode"){
       this.storkMode = "cool";
     }
      else if (mode ==="cool"){
       this.storkMode ="mysterious";
-     } 
+     }
     else{
       this.storkMode = "base mode";
     }
 
-    
+
   }
 }
